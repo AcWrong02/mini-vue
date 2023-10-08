cfbb3f0110691bae895c675d8460511d1e07cc1b
AcWrong02 <3024800887@qq.com>
2023-10-09 03:13:05 +0800

    feat:实现element初始化主流程

--- example/helloworld/App.js
+++ example/helloworld/App.js
@@ -1,7 +1,14 @@
 import { h } from "../../lib/guide-mini-vue.esm.js";
 export const App = {
   render() {
-    return h("div", "hi," + this.msg);
+    return h(
+      "div",
+      { id: "root", class: ["red", "head"] },
+      // string
+      //  "hi, mini-vue",
+      // Array
+      [h("p", { class: "red" }, "hi"), h("p", { class: "blue" }, "mini-vue")]
+    );
   },
   setup() {
     return {
--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,3 +1,4 @@
+import { isObject } from "../shared";
 import { createComponentInstance, setupComponent } from "./component";
 
 export function render(vnode, container) {
@@ -8,10 +9,40 @@ function patch(vnode, container) {
   //TODO 判断vnode是不是一个element
   //是element那么就应该处理element
   //如何去区分是element类型还是component类型？
-  processElement()
+  if (typeof vnode.type === "string") {
+    processElement(vnode, container);
+  } else if (isObject(vnode.type)) {
+    //去处理组件
+    processComponent(vnode, container);
+  }
+}
 
-  //去处理组件
-  processComponent(vnode, container);
+function processElement(vnode, container) {
+  mountElement(vnode, container);
+}
+
+function mountElement(vnode: any, container: any) {
+  const el = document.createElement(vnode.type);
+  const { children } = vnode;
+  if (typeof children === "string") {
+    el.textContent = children;
+  } else if (Array.isArray(children)) {
+    mountChlidren(vnode, el);
+  }
+
+  //props
+  const { props } = vnode;
+  for (const key in props) {
+    const val = props[key];
+    el.setAttribute(key, val);
+  }
+  container.append(el);
+}
+
+function mountChlidren(vnode, container) {
+  vnode.children.forEach((v) => {
+    patch(v, container);
+  });
 }
 
 function processComponent(vnode, container) {
