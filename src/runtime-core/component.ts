26449b3f377d64ab1799698b2d4a113c7a4fe97f
AcWrong02 <3024800887@qq.com>
2023-10-09 00:20:51 +0800

    feat:初始化component主流程

---
+++ src/runtime-core/component.ts
@@ -0,0 +1,49 @@
+export function createComponentInstance(vnode) {
+    const component = {
+        vnode,
+        type: vnode.type
+    }
+    return component
+}
+
+export function setupComponent(instance) {
+
+
+    //1.初始化props
+    // initProps()
+
+    //2.初始化slots
+    // initSlots()
+
+    setupStatefulComponent(instance);
+}
+
+function setupStatefulComponent(instance) {
+    const Component = instance.type;
+
+    const { setup } = Component;
+
+    if (setup) {
+        //注意，setup可以返回function，也可以返回Object，因此需要做不同的处理
+        const setupResult = setup();
+
+        handleSetupResult(instance, setupResult);
+    }
+}
+
+function handleSetupResult(instance, setupResult: any) {
+    //注意，setup可以返回function，也可以返回Object，因此需要做不同的处理
+    //TODO:function
+    if (typeof setupResult === "object") {
+        instance.setupState = setupResult;
+    }
+    finishComponentSetup(instance);
+}
+function finishComponentSetup(instance: any) {
+    const Component = instance.type;
+
+    if(Component.render){
+        instance.render = Component.render;
+    }
+}
+
